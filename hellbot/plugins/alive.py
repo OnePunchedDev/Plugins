eee036d9caf8660f73045126d9724ffba6c07b75
Lyfe Sredrum <101948533+OnePunchedDev@users.noreply.github.com>
2022-03-20 14:21:16 -0400

    alive message

--- hellbot/plugins/alive.py
+++ hellbot/plugins/alive.py
@@ -20,7 +20,7 @@ ALIVE_TEMP = """
 ┣─ <b>» Uptime ~</b> <i>{}</i>
 ┣─ <b>» Ping ~</b> <i>{}</i>
 ╰──────────────
-<b><i>»»» <a href='https://t.me/ExcaliburChat'>[ Excalibur ]</a> «««</i></b>
+<b><i>»»» <a href='https://t.me/its_hellbot'>[ †hê Hêllẞø† ]</a> «««</i></b>
 """
 
 msg = """{}\n
@@ -49,7 +49,7 @@ async def up(event):
                 pic_list.append(c)
         PIC = random.choice(pic_list)
     else:
-        PIC = "https://telegra.ph/file/dd89219058c65686d3f53.mp4"
+        PIC = "https://telegra.ph/file/ea9e11f7c9db21c1b8d5e.mp4"
     end = datetime.datetime.now()
     ling = (end - start).microseconds / 1000
     omk = ALIVE_TEMP.format(ForGo10God, HELL_USER, tel_ver, hell_ver, is_sudo, uptime, ling)
